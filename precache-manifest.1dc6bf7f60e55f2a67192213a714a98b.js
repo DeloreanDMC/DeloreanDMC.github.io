c12698ba68eebe7393d08cbc38813b2ead673e08
kmilovanov <kmilovanov@neoflex.ru>
2021-03-06 00:37:27 +0300

    Test optimization

---
+++ precache-manifest.1dc6bf7f60e55f2a67192213a714a98b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e42c58169fb5e646ccf053fcdce0c1b6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dc6748e56ef658642ef6",
+    "url": "/static/css/main.5e380713.chunk.css"
+  },
+  {
+    "revision": "faf62fd101868adacf52",
+    "url": "/static/js/2.66a0301a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.66a0301a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc6748e56ef658642ef6",
+    "url": "/static/js/main.bbd5278a.chunk.js"
+  },
+  {
+    "revision": "6372d17a036b023cde19",
+    "url": "/static/js/runtime-main.e5f00107.js"
+  },
+  {
+    "revision": "f46bf35183cf43dc0504fd5a70ac1f7e",
+    "url": "/static/media/hunger.f46bf351.svg"
+  }
+]);
